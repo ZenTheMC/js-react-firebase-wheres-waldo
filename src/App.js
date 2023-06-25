8827aa83fde9898db38a3bf818577badd43f4230
Zen <amaar5ali@gmail.com>
2023-06-25 13:20:20 -0400

    updated App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Leaderboards from "./pages/Leaderboards";
+import FetchTopScores from "./components/FetchTopScores";
 
 const App = () => {
   return (
     <div className="App">
-      Yo! I'm a placeholder to see if rendering of components from App component is working!
+      <Switch>
+        <Route path="/leaderboards">
+          <Leaderboards />
+        </Route>
+        <Route path="/fetch-top-scores">
+          <FetchTopScores />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
