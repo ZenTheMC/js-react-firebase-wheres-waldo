984d93d1c4d7a39e1636a369acf38d8acddb643c
Zen <amaar5ali@gmail.com>
2023-07-06 23:33:29 -0400

    lowered position of Notification message so it doesn't overlap with Navbar

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Notification = ({ message }) => {
     return (
-        <div style={{ position: 'fixed', top: '10px', right: '10px', background: 'rgba(0, 0, 0, 0.8)', color: 'white', padding: '10px', borderRadius: '5px' }}>
+        <div style={{ position: 'fixed', top: '120px', right: '10px', background: 'rgba(0, 0, 0, 0.8)', color: 'white', padding: '10px', borderRadius: '5px' }}>
             {message}
         </div>
     );
