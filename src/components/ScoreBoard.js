74bd1e31db8377e48260007eb25e439014b9eb16
Zen <amaar5ali@gmail.com>
2023-07-09 14:59:04 -0400

    added message upon score submittion

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { addScore } from '../firebase';
+import Notification from './Notification';
 
 const ScoreBoard = ({ score, username, gameTime }) => {
-    const handleSubmitScore = () => {
+    const [notification, setNotification] = useState(null);
+
+    const handleSubmitScore = async () => {
         if (username.trim() === '') {
             alert('Please enter a username before starting a new game.');
         } else {
-            addScore(username, score, gameTime);
+            try {
+                await addScore(username, score, gameTime);
+                setNotification('Score submitted');
+                setTimeout(() => setNotification(null), 2000);
+            } catch (error) {
+                setNotification('Error submitting score');
+                setTimeout(() => setNotification(null), 2000);
+            }
         }
     };
 
     return (
         <div>
+            {notification && <Notification message={notification} />}
             <div data-testid="score-display">
                 Current Score: {score}
             </div>
