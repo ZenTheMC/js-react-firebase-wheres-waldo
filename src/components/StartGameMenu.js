71040b4e8494eacdf99b4ef3ed787cf543973e1f
Zen <amaar5ali@gmail.com>
2023-07-09 22:57:55 -0400

    inline to css mod

--- src/components/StartGameMenu.js
+++ src/components/StartGameMenu.js
@@ -4,38 +4,16 @@ import Charmander from '../images/charmander.svg';
 import Eevee from '../images/eevee.svg';
 import Raichu from '../images/raichu.svg';
 import Squirtle from '../images/squirtle.svg';
+import styles from '../styles/StartGameMenu.module.css';
 
 const StartGameMenu = ({ onStart }) => {
     return (
-        <div className="start-game-menu" style={{ 
-            position: 'fixed', 
-            top: 0, 
-            left: 0,
-            right: 0,
-            bottom: 0,
-            width: '100%', 
-            height: '100%',
-            zIndex: 1000, 
-            display: 'flex', 
-            flexDirection: 'column',
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            backgroundColor: 'rgba(0, 0, 0, 0.7)',
-            color: 'yellow', 
-        }}>
-            <div style={{
-                backgroundColor: 'navy',
-                padding: '20px',
-                borderRadius: '10px',
-                textAlign: 'center',
-            }}>
+        <div className={styles.startGameMenu}>
+            <div className={styles.startGameMenuInner}>
                 <h1>Welcome to WHO'S THAT POKEMON!?</h1>
                 <h2>PLEASE PLAY IN FULL SCREEN WEBPAGE RESOLUTION</h2>
                 <p>Instructions: </p>
-                <ul style={{
-                    listStyle: 'none',
-                    padding: '10px',
-                }}>
+                <ul className={styles.instructionsList}>
                     <li>Find all 5 Pokemon within the routes</li>
                     <li>----------------------------------------------------------</li>
                     <li>Click on the Pokemon and guess it's name</li>
@@ -50,7 +28,7 @@ const StartGameMenu = ({ onStart }) => {
                     <img src={Raichu} alt="Raichu" />
                     <img src={Squirtle} alt="Squirtle" />
                 </div>
-                <button onClick={onStart} style={{ marginTop: '30px', padding: '5px', color: 'yellow', backgroundColor: 'navy', }}>Start Game</button>
+                <button onClick={onStart} className={styles.startButton}>Start Game</button>
             </div>
         </div>
     );
