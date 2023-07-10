23a9ec1fc5c29c02b74bcb17e77b693c37d5cd67
Zen <amaar5ali@gmail.com>
2023-07-10 00:27:56 -0400

    inline style to css mod

--- src/pages/Leaderboards.js
+++ src/pages/Leaderboards.js
@@ -1,11 +1,12 @@
 import React from "react";
 import FetchTopScores from "../components/FetchTopScores";
 import { Link } from 'react-router-dom';
+import styles from '../styles/Leaderboards.module.css';
 
 const Leaderboards = () => {
     return (
-        <div data-testid="leaderboards">
-            <Link to="/" style={{ backgroundColor: 'black', color: 'white', textDecoration: 'none', margintop: '20px', border: 'solid 1px', padding: '5px' }}>Go to game page</Link>
+        <div data-testid="leaderboards" className={styles.leaderboards}>
+            <Link to="/" className={styles.link}>Go to game page</Link>
             <h1>Leaderboards</h1>
             <FetchTopScores />
         </div>
