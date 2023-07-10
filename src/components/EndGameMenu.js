9a63b2e67b55d9c23c441ea266718d6bd336f51e
Zen <amaar5ali@gmail.com>
2023-07-09 21:46:44 -0400

    inline to css module

--- src/components/EndGameMenu.js
+++ src/components/EndGameMenu.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ScoreBoard from './ScoreBoard';
 import { Link } from 'react-router-dom';
+import styles from '../styles/EndGameMenu.module.css';
 
 const EndGameMenu = ({ startNewGame, score, gameTime }) => {
   const [username, setUsername] = useState('');
@@ -10,11 +11,11 @@ const EndGameMenu = ({ startNewGame, score, gameTime }) => {
   };
 
   return (
-    <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'navy', padding: '20px', borderRadius: '10px', zIndex: 1000 }}>
+    <div className={styles.menu}>
       <input type="text" placeholder="Enter username" value={username} onChange={handleUsernameChange} />
       <ScoreBoard score={score} startNewGame={startNewGame} username={username} gameTime={gameTime} />
-      <button onClick={startNewGame} style={{ background: 'navy', color: 'yellow', textDecoration: 'none', display: 'block', marginTop: '20px', border: 'solid 1px', padding: '5px' }}>Close Menu</button>
-      <Link to="/leaderboards" style={{ color: 'yellow', textDecoration: 'none', display: 'block', marginTop: '20px', border: 'solid 1px', padding: '5px' }}>Go to Leaderboards</Link>
+      <button onClick={startNewGame} className={styles.menuButton}>Close Menu</button>
+      <Link to="/leaderboards" className={styles.menuLink}>Go to Leaderboards</Link>
     </div>
   );
 };
