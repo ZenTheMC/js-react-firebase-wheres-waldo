61796f5def63abd7bc722abd8f595ab24cc6a053
Zen <amaar5ali@gmail.com>
2023-07-09 22:35:53 -0400

    box-sizing:border-box universal styling to index component, inline styles to module Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,22 @@ import Charmander from '../images/charmander.svg';
 import Eevee from '../images/eevee.svg';
 import Raichu from '../images/raichu.svg';
 import Squirtle from '../images/squirtle.svg';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = ({ score, isGameOver, isGameStarted }) => {
   return (
-    <nav style={{ position: 'fixed', top: 0, width: '99%', zIndex: 1000, display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: 'navy', color: 'yellow', }}>
+    <nav className={styles.navbar}>
       <h1>WHERE'S THAT POKEMON!?</h1>
       <h2><strong>PLEASE PLAY IN FULL SCREEN WEBPAGE</strong></h2>
-      <div style={{ display: 'flex', alignItems: 'center', }}>
+      <div className={styles.navbarItems}>
         {isGameStarted && <Timer isGameOver={isGameOver} isGameStarted={isGameStarted} />}
-        <img src={Bulbasaur} alt="Bulbasaur" style={{ width: '50px', height: '50px', marginRight: '5px', marginLeft: '50px', }} />
-        <img src={Charmander} alt="Charmander" style={{ width: '50px', height: '50px', marginRight: '5px', }} />
-        <img src={Eevee} alt="Eevee" style={{ width: '50px', height: '50px', marginRight: '5px', }} />
-        <img src={Raichu} alt="Raichu" style={{ width: '50px', height: '50px', marginRight: '5px', }} />
-        <img src={Squirtle} alt="Squirtle" style={{ width: '50px', height: '50px', marginRight: '50px', }} />
-        <div style={{ marginRight: '20px', border: 'solid 1px', padding: '5px', }}>Score: {score}</div>
-        <Link to="/leaderboards" style={{ color: 'yellow', textDecoration: 'none', marginLeft: '20px', border: 'solid 1px', padding: '5px', }}>Leaderboards Page</Link>
+        <img src={Bulbasaur} alt="Bulbasaur" className={styles.pokemonImage} />
+        <img src={Charmander} alt="Charmander" className={styles.pokemonImage} />
+        <img src={Eevee} alt="Eevee" className={styles.pokemonImage} />
+        <img src={Raichu} alt="Raichu" className={styles.pokemonImage} />
+        <img src={Squirtle} alt="Squirtle" className={styles.pokemonImage} />
+        <div className={styles.score}>Score: {score}</div>
+        <Link to="/leaderboards" className={styles.link}>Leaderboards Page</Link>
       </div>
     </nav>
   );
