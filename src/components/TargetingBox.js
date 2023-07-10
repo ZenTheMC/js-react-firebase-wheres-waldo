b30228d27561ed04863e9c6d2b3a2b7e577f264f
Zen <amaar5ali@gmail.com>
2023-07-09 23:05:45 -0400

    static inline -> css mod

--- src/components/TargetingBox.js
+++ src/components/TargetingBox.js
@@ -1,27 +1,20 @@
 import React from "react";
 import CharacterSelection from "./CharacterSelection";
+import styles from '../styles/TargetingBox.module.css';
 
 const TargetingBox = ({ position, characters, incrementScore }) => {
     const boxWidth = 135;
     const boxHeight = 20;
 
     const targetingBoxStyle = {
-        position: 'absolute',
         top: position.y - boxHeight / 2,
         left: position.x - boxWidth / 2,
-        border: '2px solid red',
-        backgroundColor: 'white',
-        padding: '5px',
         width: boxWidth,
         height: boxHeight,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: 10
     };
 
     return (
-        <div data-testid="targeting-box" style={targetingBoxStyle}>
+        <div data-testid="targeting-box" className={styles.targetingBox} style={targetingBoxStyle}>
             <CharacterSelection position={position} characters={characters} incrementScore={incrementScore} />
         </div>
     );
