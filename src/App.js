5e985e46c7e77f044213d518dd3cf2491adbaf47
Zen <amaar5ali@gmail.com>
2023-07-11 19:40:42 -0400

    deploy debug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Game from './pages/Game';
 import Leaderboards from './pages/Leaderboards';
 import styles from './styles/App.module.css';
